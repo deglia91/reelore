f0c9409352ba46fa6e78ee74465aeebc14622d9d
deglia91 <50019116+deglia91@users.noreply.github.com>
2026-08-21 16:38:04 +0200

    feat(web): compose shared mobile shell

--- src/reelore/__init__.py
+++ src/reelore/__init__.py
@@ -2,5 +2,6 @@
 
 from reelore import web_navigation_theme as _web_navigation_theme
 from reelore.web_brand_polish import BRAND_POLISH_CSS
+from reelore.web_shell import NEXT_EP_SHELL_CSS
 
-_web_navigation_theme.NAVIGATION_CSS += BRAND_POLISH_CSS
+_web_navigation_theme.NAVIGATION_CSS += BRAND_POLISH_CSS + NEXT_EP_SHELL_CSS
