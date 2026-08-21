2c5008d27f7240dc2ab8f2dc30f48e2d1493dfb5
deglia91 <50019116+deglia91@users.noreply.github.com>
2026-08-21 16:55:15 +0200

    feat(web): compose mobile shell polish

--- src/reelore/__init__.py
+++ src/reelore/__init__.py
@@ -3,5 +3,6 @@
 from reelore import web_navigation_theme as _web_navigation_theme
 from reelore.web_brand_polish import BRAND_POLISH_CSS
 from reelore.web_shell import NEXT_EP_SHELL_CSS
+from reelore.web_shell_polish import SHELL_POLISH_CSS
 
-_web_navigation_theme.NAVIGATION_CSS += BRAND_POLISH_CSS + NEXT_EP_SHELL_CSS
+_web_navigation_theme.NAVIGATION_CSS += BRAND_POLISH_CSS + NEXT_EP_SHELL_CSS + SHELL_POLISH_CSS
